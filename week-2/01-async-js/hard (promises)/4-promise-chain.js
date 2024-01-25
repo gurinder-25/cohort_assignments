bfc221cbb8a555b69fea79eb40854d19bbdc4be0
Gurinder <132286862+gurinder-25@users.noreply.github.com>
2024-01-25 19:49:24 +0530

    Update 4-promise-chain.js

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,34 @@
  */
 
 function wait1(t) {
-
+    return new Promise(function(resolve){
+        setTimeout(resolve, t*1000)
+    })
 }
 
 function wait2(t) {
-
+    return new Promise(function(resolve){
+        setTimeout(resolve, t*1000)
+    })
 }
 
 function wait3(t) {
-
+    return new Promise(function(resolve){
+        setTimeout(resolve, t*1000)
+    })
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const Start_Time = new Date().getTime();
+    const ans = wait1(t1).then(function(){
+        return wait2(t2);
+    }).then(function(){
+        return wait3(t3);
+    }).then(function(){
+        const End_Time = new Date().getTime() - Start_Time;
+        return End_Time;
+    })
+    return ans;
 }
 
 module.exports = calculateTime;
